f352d0654315ee3f1a23cbebe15130073c4544c7
Soham Patel <sohamhp@uci.edu>
2025-02-27 14:29:41 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (53.94%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -5,13 +5,15 @@ class Solution:
         if len(nums)==1:
             return nums[0]
             
-        arr=[0]*len(nums)
-        arr[0]=nums[0]
-        arr[1]=max(nums[0],nums[1])
+        
+        prev1=nums[0]
+        prev2=max(nums[0],nums[1])
         maxi=0
 
         for i in range(2,len(nums)):
-            arr[i]= max(nums[i]+arr[i-2],arr[i-1])
+            temp=prev2
+            prev2=max((nums[i]+ prev1),prev2)
+            prev1=temp
         
-        return arr[-1]
+        return prev2
                 

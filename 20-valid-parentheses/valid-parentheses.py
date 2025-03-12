38816d6cb353c31b2ce50610e4dee0526f96fbda
Soham Patel <sohamhp@uci.edu>
2025-03-11 23:14:17 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (32.41%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        p = {")": "(", "}": "{", "]": "["}
+        stack = []
+
+        for i in s:
+            if i in p:
+                if not stack or stack[-1]!=p[i]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(i)
+
+        return len(stack)==0
+        

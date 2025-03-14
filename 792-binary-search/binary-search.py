6dca48bf753529a8ff9553fd20ca53ffbc6e51af
Soham Patel <sohamhp@uci.edu>
2025-03-14 00:01:20 -0700

    Time: 0 ms (100.00%) | Memory: 18.7 MB (73.91%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -3,11 +3,12 @@ class Solution:
         r=len(nums)-1
         l=0
         while l<=r:
-            if target==nums[(l+r)//2]:
-                return (l+r)//2
-            if target>nums[(l+r)//2]:
-                l=((l+r)//2)+1
-            if target<nums[(l+r)//2]:
-                r=((l+r)//2)-1
+            m=(l+r)//2
+            if target==nums[m]:
+                return m
+            if target>nums[m]:
+                l=m+1
+            if target<nums[m]:
+                r=m-1
             
         return -1

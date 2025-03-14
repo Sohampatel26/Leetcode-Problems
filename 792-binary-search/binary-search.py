cda2ce9ee1cfdfe85184c9e8505072d019a88d9a
Soham Patel <sohamhp@uci.edu>
2025-03-13 23:56:43 -0700

    Time: 0 ms (100.00%) | Memory: 18.7 MB (45.20%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,11 +1,13 @@
-class Solution(object):
-    def search(self, nums, target):
-        l,r=0,len(nums)-1
-        while  l<=r:
-            if target==nums[(r+l)//2]:
-                return (r+l)//2
-            elif target>nums[(r+l)//2]:
-                l=((r+l)//2)+1
-            elif target<nums[(r+l)//2]:
-                r=((r+l)//2)-1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        r=len(nums)-1
+        l=0
+        while l<=r:
+            if target==nums[(l+r)//2]:
+                return (l+r)//2
+            if target>nums[(l+r)//2]:
+                l=((l+r)//2)+1
+            if target<nums[(l+r)//2]:
+                r=((l+r)//2)-1
+            
         return -1

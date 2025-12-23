8d473c195d8ec2afa92d4d74fde8744ad4fad386
Soham Patel <sohamhp@uci.edu>
2025-12-22 18:35:36 -0600

    Time: 923 ms (17.80%) | Memory: 20 MB (99.06%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -5,24 +5,21 @@ class Solution:
         for i in range(len(nums)):
             if i>0 and nums[i]==nums[i-1]:
                 continue
-            l=i+1
-            r=len(nums)-1
-            while l<r:
-                if nums[l]+nums[r]+nums[i]==0:
-                    res.append([nums[i],nums[l],nums[r]])
-                    while l<r and nums[l]==nums[l+1]:
-                        l+=1
-                    while l<r and nums[r]==nums[r-1]:
-                        r-=1
-                    l+=1
-                    r-=1
+            
+            j=i+1
+            k=len(nums)-1
+            while j<k:
+                if nums[i]+nums[j]+nums[k]==0:
+                    res.append([nums[i],nums[j],nums[k]])
+                    j+=1
+                    while nums[j]==nums[j-1] and j<k:
+                        j+=1
                     
-                elif nums[l]+nums[r]+nums[i]>0:
-                    r-=1
-                else:
-                    l+=1
-
+                    k-=1
+                
+                elif nums[i]+nums[j]+nums[k]>0:
+                    k-=1
+                
+                elif nums[i]+nums[j]+nums[k]<0:
+                    j+=1
         return res
-
-        
-

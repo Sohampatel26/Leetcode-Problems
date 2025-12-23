8b129d1ce18fd7fae553a708f56729cf44d5267b
Soham Patel <sohamhp@uci.edu>
2025-12-23 16:58:33 -0600

    Time: 23 ms (19.17%) | Memory: 21.8 MB (35.35%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,10 +1,11 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        sol=defaultdict(list)
-        for i in strs:
-            count=[0]*26
-            for j in i:
-                count[ord(j)-ord('a')]+=1
-            sol[tuple(count)].append(i)
-        return sol.values()
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        se=defaultdict(list)
 
+        for s in strs:
+            count=[0]*26
+            for i in s:
+                count[ord(i)-ord('a')]+=1
+            se[tuple(count)].append(s)
+        
+        return list(se.values())

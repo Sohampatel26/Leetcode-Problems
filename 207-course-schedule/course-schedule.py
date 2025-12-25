0bb5a05992cc7f7e035e2f346fe0c2327c27e490
Soham Patel <sohamhp@uci.edu>
2025-12-25 13:07:19 -0600

    Time: 9 ms (17.39%) | Memory: 18.6 MB (96.96%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,27 +1,29 @@
-
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        visit=[0]*numCourses
         graph=defaultdict(list)
+        visited=[0]*numCourses
+
         for a,b in prerequisites:
             graph[b].append(a)
         
-        def dfs(node):
-            if visit[node]==1:
+        def dfs(i):
+            if visited[i]==1:
                 return False
-            if visit[node]==2:
+            
+            if visited[i]==2:
                 return True
             
-            visit[node]=1
-            for c in graph[node]:
-                if not dfs(c):
+            visited[i]=1
+            for nei in graph[i]:
+                if not dfs(nei):
                     return False
             
-            visit[node]=2
+            visited[i]=2
             return True
-        
-        for i in range(numCourses):
-            if not dfs(i):
-                return False
-        return True
 
+        for a in range(numCourses):
+            if not dfs(a):
+                return False
+        
+        return True
+        
